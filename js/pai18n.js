cf5e1c8af7cb07a9cc32bb9b0a483304a2604c46
Anomalyce <47085833+anomalyce@users.noreply.github.com>
2023-03-02 00:12:08 +0100

    WIP

--- js/pai18n.js
+++ js/pai18n.js
@@ -1,13 +1,18 @@
+import { ref, inject } from 'vue'
 export { Translation as VueComponent } from './VueComponent.js'
 
 /**
  * Translate a message.
  *
- * @param  string  message
+ * @param  string  path
  * @param  object  params
  * @return string
  */
-function translate(message, params) {
+function translate(options, path, params) {
+  const translations = options.translations[options.locale.value] || {}
+
+  let message = translations[path] || path
+
   Object.entries(params || {}).map(([k, v]) => {
     message = message.replace(new RegExp(`:${k}`, 'i'), v)
   })
@@ -15,9 +20,26 @@ function translate(message, params) {
   return message
 }
 
-export const usePai18n = () => ({
-  translate,
-  __: translate,
-})
+export const usePai18n = () => {
+  const options = inject('pai18n')
+
+  return {
+    locale: options?.locale,
+    translations: options?.translations,
+    translate: (...args) => Reflect.apply(translate, undefined, [options, ...args]),
+    __: (...args) => Reflect.apply(translate, undefined, [options, ...args]),
+  }
+}
+
+export const createPai18n = {
+  install(app, options) {
+    const locale = ref(options.locale)
+
+    app.provide('pai18n', {
+      locale,
+      translations: options.translations,
+    })
+  }
+}
 
 export default usePai18n
--- src/Providers/Pai18nServiceProvider.php
+++ src/Providers/Pai18nServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Anomalyce\Pai18n\Providers;
 
+use Illuminate\Support\Arr;
 use Anomalyce\Pai18n\Commands;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\AggregateServiceProvider;
 
 class Pai18nServiceProvider extends AggregateServiceProvider
@@ -14,7 +16,9 @@ class Pai18nServiceProvider extends AggregateServiceProvider
    */
   public function boot()
   {
-    //
+    Blade::directive('pai18n', function ($string) {
+      return "<?php echo app('pai18n')->generateHtml(); ?>";
+    });
   }
 
   /**
@@ -24,6 +28,63 @@ class Pai18nServiceProvider extends AggregateServiceProvider
    */
   public function register()
   {
+    $this->app->singleton('pai18n', function ($app) {
+      return new class($app) {
+        protected string $locale;
+        protected string $fallbackLocale;
+        protected array $locales;
+
+        public function __construct(protected $app) {
+          $this->locale = $this->app->getLocale();
+          $this->fallbackLocale = $this->app->getFallbackLocale();
+          $this->locales = array_unique([$this->fallbackLocale]);
+        }
+
+        protected function getTranslations(string $path, ?string $locale = null): array
+        {
+          $items = trans($path, [], $locale ?: $this->fallbackLocale, true);
+
+          return $items !== $path ? $items : [];
+        }
+
+        protected function getMessages(): array
+        {
+          $messages = [];
+
+          foreach ($this->locales as $locale) {
+            $messages[$locale] = array_filter($this->getTranslations('*', $locale), fn ($value) => ! empty($value));
+          }
+
+          return $messages;
+        }
+
+        public function toArray(): array {
+          return [
+            'locale' => $this->locale,
+            'translations' => $this->getMessages(),
+          ];
+        }
+
+        public function toJson(): string {
+          return json_encode($this->toArray(), JSON_UNESCAPED_UNICODE);
+        }
+
+        public function generateJavaScript() {
+          return <<<JS
+            const pai18n = {$this->toJson()};
+          JS;
+        }
+
+        public function generateHtml() {
+          return <<<HTML
+            <script>
+              {$this->generateJavaScript()}
+            </script>
+          HTML;
+        }
+      };
+    });
+
     $this->commands([
       Commands\Pai18nScanCommand::class,
     ]);
